9e2d502b61cd26fa556b6b847e78546c6f12e0b4
Halekio <dany.bolduc@live.com>
2020-11-18 19:03:44 -0500

    Add files via upload
    Commit #1

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1605743965,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/board.png",
+		"images/blackbg.png",
+		"images/fruits-sheet0.png",
+		"images/pacman-sheet0.png",
+		"images/spritefont.png",
+		"images/ghost-sheet0.png",
+		"images/ghost-sheet1.png",
+		"images/bigdotflasher-sheet0.png",
+		"images/life-sheet0.png",
+		"images/life-sheet1.png",
+		"images/levelfruit-sheet0.png",
+		"images/levelfruit-sheet1.png"
+	]
+}
